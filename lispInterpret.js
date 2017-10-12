2bb9e92f2ceba46c192f5148b2aba2f11e07446e
Neethu Mohandas <prayaganeethu@gmail.com>
2017-10-12 08:48:45 +0530

    Expression parser works for nested expressions, but bugs coming up for some test cases

--- lispInterpret.js
+++ lispInterpret.js
@@ -1,10 +1,9 @@
 let env = {
   // '+': function (arg) { arg.reduce(function (sum, value) { return sum + value }) }
-  '+': function (arg) { return arg[0] + arg[1] },
-  '-': function (arg) { return arg[0] - arg[1] },
-  '*': function (arg) { return arg[0] * arg[1] },
-  '/': function (arg) { return arg[0] / arg[1] },
-  '%': function (arg) { return arg[0] + arg[1] },
+  '+': (arg) => arg.reduce((sum, val) => sum + val),
+  '-': (arg) => arg.reduce((dif, val) => dif - val),
+  '*': (arg) => arg.reduce((mul, val) => mul * val),
+  '/': (arg) => arg.reduce((div, val) => div / val),
   '<': function (arg) { return arg[0] < arg[1] },
   '>': function (arg) { return arg[0] > arg[1] },
   '<=': function (arg) { return arg[0] <= arg[1] },
@@ -12,8 +11,8 @@ let env = {
   'pow': function (arg) { return Math.pow(arg[0], arg[1]) },
   'pi': function (arg) { return Math.PI },
   'length': function (arg) { return arg[0].length },
-  'max': function (arg) { return Math.max(arg) },
-  'min': function (arg) { return Math.min(arg) },
+  'max': (arg) => arg.reduce((max, cur) => max > cur ? max : cur),
+  'min': (arg) => arg.reduce((min, cur) => min < cur ? min : cur),
   'eq?': function (arg) { return arg[0] == arg[1] },
   'equal?': function (arg) { return arg[0] === arg[1] },
   'sqrt': function (arg) { return arg[0] },
@@ -27,7 +26,7 @@ exports.interpretLisp = function (lispInput) {
   return (lispInput) ? lispParser(lispInput) : null
 }
 
-let lispParser = factoryParser(defineParser, expressionParser, symbolParser, literalParser)
+let lispParser = factoryParser(expressionParser, defineParser, opParser, literalParser, symbolParser)
 
 function factoryParser (...parsers) {
   return function (In) {
@@ -40,37 +39,75 @@ function factoryParser (...parsers) {
 }
 
 function expressionParser (lispInput) {
-  console.log('Expression')
-  let lispy = ''
-  let i = 1
-  if (spaceParser(lispInput)[0] === '(') {
-    while (lispInput[i] !== ')') {
-      lispy += lispInput[i]
-      i++
-    }
+  // console.log('Expression')
+  if (lispInput[0] === '(') {
+    return lispParser(spaceParser(lispInput.slice(1)))
   }
-  lispy = spaceParser(lispy)
+  return null
+}
+
+function defineParser (lispInput) {
+  // console.log('define')
+  if (lispInput.slice(0, 6) === 'define') {
+    let res = symbolParser(spaceParser(lispInput.slice(7)))
+    let symbol = res[0]
+    console.log('hey', res[1])
+    let val = lispParser(spaceParser(res[1]))
+    console.log('val', val[0])
+    env[symbol] = val[0]
+    console.log(env)
+    return [null, val[1]]
+  }
+  return null
+}
+
+function opParser (lispInput) {
+  let op = lispInput.split(' ')[0]
+  console.log(op)
+  let j = op.length
+  let arg = ''
   let argArr = []
-  let args = lispy.split(' ')
-  let op = args[0]
-  for (let i = 1; i < args.length; i++) {
-    if (args[i] !== ' ') {
-      argArr.push(parseFloat(args[i]))
-    }
-  }
-  let res
+  lispInput = spaceParser(lispInput.slice(j + 1))
+  console.log(lispInput)
   if (op in env) {
-    res = env[op](argArr)
+    while (lispInput) {
+      if (lispInput[0] !== ' ' && lispInput[0] !== ')' && lispInput[0] !== '(') {
+        arg += lispInput[0]
+        lispInput = lispInput.slice(1)
+      }
+      else if (lispInput[0] === '(') {
+        arg = lispParser(lispInput)
+        let args = arg[0]
+        lispInput = arg[1]
+        argArr.push(parseFloat(args))
+        lispInput = lispInput.slice(1)
+        arg = ''
+      }
+      else if (lispInput[0] === ' ') {
+        argArr.push(parseFloat(arg))
+        lispInput = lispInput.slice(1)
+        arg = ''
+      }
+      else if (lispInput[0] === ')') {
+        if (arg) {
+          argArr.push(parseFloat(arg))
+          arg = ''
+        }
+        lispInput = lispInput.slice(1)
+      }
+      console.log('argArr', argArr)
+    }
+    return [env[op](argArr), lispInput]
   }
-  return res
+  return null
 }
 
 function symbolParser (lispInput) {
-  console.log('symbol', lispInput)
+  // console.log('symbol', lispInput)
   let symbol = ''
   let i = 0
   if (/^[a-zA-Z-]/.test(lispInput[i])) {
-    while (lispInput[i] !== ' ') {
+    while (lispInput[i] !== ' ' && lispInput[i] !== ')') {
       symbol += lispInput[i]
       i++
     }
@@ -80,15 +117,15 @@ function symbolParser (lispInput) {
 }
 
 function literalParser (lispInput) {
-  console.log('literal')
+  // console.log('literal')
   let num = ''
   let i = 0
   if (/[0-9]+/.test(lispInput[i])) {
-    while (/[0-9]+/.test(lispInput[i])) {
+    while (/[0-9]+/.test(lispInput[i]) && lispInput[i] !== ')') {
       num += lispInput[i]
       i++
     }
-    console.log([num, spaceParser(lispInput.slice(i))])
+    // console.log([num, spaceParser(lispInput.slice(i))])
     return [num, spaceParser(lispInput.slice(i))]
   }
   return null
@@ -97,18 +134,4 @@ function literalParser (lispInput) {
 function spaceParser (lispInput) {
   while (/\s/.test(lispInput[0])) lispInput = lispInput.slice(1)
   return lispInput
-}
-
-function defineParser (lispInput) {
-  console.log('define')
-  if (lispInput.slice(0, 6) === 'define') {
-    let res = symbolParser(spaceParser(lispInput.slice(7)))
-    let symbol = res[0]
-    console.log('hey', res[1])
-    let val = lispParser(spaceParser(res[1]))
-    console.log('val', val[0])
-    env[symbol] = val[0]
-    return env
-  }
-  return null
 }
